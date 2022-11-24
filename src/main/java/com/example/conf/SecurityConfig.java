18f77913a2b5d69e73cff111238548cc56c1723d
veiiev <worldloflwarcraft@mail.ru>
2022-11-24 04:11:10 +0400

    начинаю работать с валидацией в классе user, в последний раз из-за этого все сломалось, rollbackPoint

--- src/main/java/com/example/conf/SecurityConfig.java
+++ src/main/java/com/example/conf/SecurityConfig.java
@@ -1,15 +1,14 @@
 package com.example.conf;
 
 
-import org.springframework.context.EmbeddedValueResolverAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -35,4 +34,6 @@ public class SecurityConfig  {
                 .csrf().disable();
         return http.build();
     }
+
+
 }
--- src/main/java/com/example/controller/rest/RestRegistrationController.java
+++ src/main/java/com/example/controller/rest/RestRegistrationController.java
@@ -8,22 +8,26 @@ import org.apache.coyote.Response;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("/api/registration")
 @AllArgsConstructor
+@Validated
 public class RestRegistrationController {
 
     UserDetailsServiceImpl userDetailsService;
 
 
     @PostMapping("/signup")
-    public ResponseEntity<User>  create(@RequestBody User user){
+    public ResponseEntity<User>  create(@Valid @RequestBody User user){
 
         if(userDetailsService.saveUser(user)==false) return new ResponseEntity<>(user, HttpStatus.BAD_REQUEST);
 
